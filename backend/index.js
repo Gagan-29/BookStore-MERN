5e4a93833a8ad7ffdbbf80294d0d53f275796218
Gagan-29 <122251921+Gagan-29@users.noreply.github.com>
2024-09-01 16:27:56 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://book-store-mern-client-lac.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
